7763a141262224a9769f7a292f965177cb4f83b1
Jungsub Ryoo <RyooJu@rosslynacademy.local>
2018-01-30 08:53:30 +0300

    PRG 3.2

---
+++ PRG 3.2 Class Registration/class_register.rb
@@ -0,0 +1,39 @@
+class_list = []
+
+puts "-------------"
+puts "|"
+puts "| STUDENT REGISTRATION"
+puts "|"
+puts "-------------"
+
+while true
+  print "Do you wish to (a)dd a student, (p)print the roster or (e)exit: "
+  input = gets.chomp
+  
+  if input.downcase == "a"
+    puts "Enter a student name"
+    print " >  "
+    input_name = gets.chomp
+    unless input_name == ""
+      class_list << input_name
+    else
+      puts "No Text Detected"
+    end
+    
+  elsif input.downcase == "p"
+    puts "-------------"
+    puts "|"
+    puts "| CLASS ROSTER"
+    puts "|"
+    puts "-------------"
+    
+    class_list.each do |name|
+      puts name
+    end
+    puts "Total Students: #{class_list.length}"
+  elsif input.downcase == "e"
+    exit
+  else
+    puts "Error. Unknown argument. Please use 'a' - add,   'p' - print,   or   'e' - exit"
+  end
+end
